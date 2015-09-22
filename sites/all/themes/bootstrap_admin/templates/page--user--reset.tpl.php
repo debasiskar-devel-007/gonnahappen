496400fbe29c5d328493b676e8a3297a51ac89c1
debasiskar-devel-007 <debasiskar007@gmail.com>
2015-09-22 18:45:09 +0530

    3.27 PM 22 th sept !

--- sites/all/themes/bootstrap_admin/templates/page--user--reset.tpl.php
+++ sites/all/themes/bootstrap_admin/templates/page--user--reset.tpl.php
@@ -1,30 +1,39 @@
+
+
+
 <div class="login-box">
     <div class="login-logo">
         <a href="<?php echo $GLOBALS['base_url']; ?>"><img src="<?php echo $logo; ?>" /></a>
     </div><!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+        <p class="login-box-msg login-box-msgtxt"><span>Continue resetting your password here !</span></p>
 
 
-        <?php
-        // print $messages;
+        This is a one-time login  and will expire in 24 hours
 
+        Click on this button to log in to the site and change your password.
 
-        $elements = drupal_get_form("user_reset");
-        $form = drupal_render($elements);
-        echo $form;
-        ?>
+        This login can be used only once.
+        <br>
+        <br>
+        <br>
+        <div class="clearfix"></div>
 
-
-
-
-
-
+        <a href="<?php echo request_uri() ; ?>/login" <button class="btn btn-primary btn-block btn-flat ">Login</button></a>
 
 
 
-       <!-- <a href="user/password">I forgot my password</a><br>-->
-        <!--<a href="register.html" class="text-center">Register a new membership</a>-->
+
+
+
+
+
+
+
+        <!-- <a href="#">I forgot my password</a><br>
+         <a href="register.html" class="text-center">Register a new membership</a>-->
 
     </div><!-- /.login-box-body -->
 </div>
+
+
